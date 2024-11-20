0e873c90d5a7c8a5866a2615eed140e895106d1e
Ojspain <140548765+Ojspain@users.noreply.github.com>
2024-11-20 13:00:10 -0800

    Update final.js

--- js/final.js
+++ js/final.js
@@ -15,10 +15,43 @@ document.addEventListener('DOMContentLoaded', function() {
 
             data.forEach(item => {
                 const section = document.createElement('section');
-                let imagesHtml = '';
-                item.images.forEach(image => {
-                    imagesHtml += `<img src="${image.url}" alt="${image.alt}" style="width:100%;margin:10px 0;">`;
-                });
+
+                    let imagesHtml = '';
+                        imagesHtml += `
+                            <div id="carouselWithControls" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                        `;
+                        item.images.forEach((image, index) => {
+                            //imagesHtml += `<img src="${image.url}" alt="${image.alt}" style="width:100%;margin:10px 0;">`;
+                            if(index == 0){
+                                imagesHtml += `
+                                <div class="carousel-item active">
+                                <img class="d-block w-100" src="${image.url}" alt="${image.alt}">
+                                </div>
+                                `;
+
+                            }else {
+                                imagesHtml += `
+                                <div class="carousel-item">
+                                <img class="d-block w-100" src="${image.url}" alt="${image.url}">
+                                </div>
+                                `;
+
+                            }
+
+                        });
+                        imagesHtml += `
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselWithControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselWithControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                            </div>
+                        `;
 
                 section.innerHTML = `
                     <h2>${item.name}</h2> 
